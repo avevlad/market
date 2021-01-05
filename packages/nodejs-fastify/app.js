e85c2df82d65dff7b4acd5b263bb575db6b77a4c
avevlad <vlad.open.source@gmail.com>
2021-01-06 00:54:26 +0300

    fighting with vercel deploy

--- packages/nodejs-fastify/app.js
+++ packages/nodejs-fastify/app.js
@@ -3,10 +3,11 @@ const fastify = require("fastify");
 function init() {
   const app = fastify();
   app.get("/", (request, reply) => reply.send({ hello: "world" }));
+  app.get("/api", (request, reply) => reply.send({ hello: "world" }));
+  app.get("/api/item/:slug", (request, reply) =>
+    reply.send({ hello: "world" })
+  );
   return app;
 }
 
-init().listen(3000, (err) => {
-  if (err) console.error(err);
-  console.log("server listening on 3000");
-});
+module.exports = init;
